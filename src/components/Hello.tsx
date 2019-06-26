1cd67e1a37d86f79257a0149e485bf1494f90191
Andy.L.Jin <Andy.L.Jin@newegg.com>
2019-06-26 18:16:42 +0800

    init

---
+++ src/components/Hello.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export interface HelloPorps {
+    compiler : string;
+    framework : string;
+}
+
+export const Hello = (props : HelloPorps) => <h1>Hello from {props.compiler} and {props.framework}!!!!!!!!!</h1>
---
+++ webpack.config.ts
@@ -0,0 +1,72 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+import webpack from 'webpack';
+import path from 'path';
+const config = {
+    mode: process.env.NODE_ENV !== 'production'?'development':'production',
+    devtool: "cheap-eval-source-map",
+    entry: process.env.NODE_ENV === 'production'?[path.resolve('src/main.tsx')]:['webpack-hot-middleware/client?reload=true', path.resolve('src/main.tsx')],
+    output : {
+        path: path.join(__dirname, '/dist/'),
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            template : path.resolve('src/index.tpl.html'),
+            inject : 'body',
+            filename : 'index.html'
+        }), 
+        new webpack.HotModuleReplacementPlugin(),  
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+        }),
+        new CleanWebpackPlugin() 
+    ],
+    optimization : {
+        
+    }
+}
+
+if(process.env.NODE_ENV === 'production'){
+    delete config['devtool']
+    config.optimization = {
+        splitChunks: {
+            chunks: 'initial',
+            minSize: 20000,
+            minChunks: 2,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            name: true,
+            cacheGroups: {
+              default: {
+                minChunks: 2,
+                priority: -20,
+              },
+              vendors: {
+                test: /[\\/]node_modules[\\/]/,
+                priority: -10,
+              },
+            }
+        }
+    }
+}
+
+module.exports = config
