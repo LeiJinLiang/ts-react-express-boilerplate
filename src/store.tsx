729a65cbf04a4af597acbb8f169f4f05431d77f6
Andy.L.Jin <Andy.L.Jin@newegg.com>
2019-06-28 16:26:00 +0800

    format

--- src/store.tsx
+++ src/store.tsx
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import { enthusiasm } from "./reducers/index";
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
-const middleware = applyMiddleware(thunk,logger);
+const middleware = applyMiddleware(thunk, logger);
 
-export const store = createStore<any, any, any, any>(enthusiasm, {
-  enthusiasmLevel: 1,
-  languageName: "TS"
-} as any,middleware);
+export const store = createStore<any, any, any, any>(
+  enthusiasm,
+  {
+    enthusiasmLevel: 1,
+    languageName: "TS"
+  } as any,
+  middleware
+);
