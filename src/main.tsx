0c709fd2ada344cb5082e9dfb4169119465066b1
Andy.L.Jin <Andy.L.Jin@newegg.com>
2019-06-28 14:29:35 +0800

    format code

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Hello from "./containers/Hello";
-import { Provider } from 'react-redux';
-import { store } from './store';
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
-      <Hello />
+    <Hello />
   </Provider>,
   document.getElementById("root") as HTMLElement
 );
--- src/store.tsx
+++ src/store.tsx
@@ -1,7 +1,7 @@
-import { createStore } from 'redux';
-import { enthusiasm } from './reducers/index';
+import { createStore } from "redux";
+import { enthusiasm } from "./reducers/index";
 
-export const store = createStore<any,any,any,any>(enthusiasm, {
-    enthusiasmLevel: 1,
-    languageName: 'TS'
+export const store = createStore<any, any, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: "TS"
 } as any);
