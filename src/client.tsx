dcfacdc80303ab134cb38443a0fb9001b72bcfed
Andy.L.Jin <Andy.L.Jin@newegg.com>
2019-07-11 15:26:24 +0800

    effect of LazyImage

--- src/client.tsx
+++ src/client.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import asyncComponent from "./asyncComponent";
 import { Home } from "./components/Home";
+import { LazyImage } from "./components/LazyImage/LazyImage";
+import "./styles/app.css";
 const Hello = asyncComponent(() =>
   import(/* webpackChunkName: 'about' */ "./containers/Hello")
 );
@@ -11,19 +13,25 @@ const RemoteContainer = asyncComponent(() =>
 import { NotFound } from "./components/404";
 export const Client = () => (
   <Router>
-    <p>
-      <Link to="/">Home</Link>
-    </p>
-    <p>
-      <Link to="/about">About</Link>
-    </p>
-    <p>
-      <Link to="/books">Books</Link>
-    </p>
+    <div className="nav">
+      <p>
+        <Link to="/">Home</Link>
+      </p>
+      <p>
+        <Link to="/about">About</Link>
+      </p>
+      <p>
+        <Link to="/books">Books</Link>
+      </p>
+      <p>
+        <Link to="/picture">picture</Link>
+      </p>
+    </div>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/about" component={Hello} />
       <Route path="/books" component={RemoteContainer} />
+      <Route path="/picture" component={LazyImage} />
       <Route component={NotFound} />
     </Switch>
   </Router>
